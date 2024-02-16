9ad26412dde766377c7595a1d9374967f9c1ea04
lizeth9797 <karinaavao@gmail.com>
2024-02-16 17:09:12 -0600

    se sube la prueba de liveCoding, ejercicios 1 y 2

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// Escribe tu código aquí.
+
+arreglo = ["adios","marcos","lizeth","caballo","generation","tres","uno"];
+cadena = "hola";
+ 
+function Ej1(cadena, arreglo) {
+    respuesta = [];
+    tamañoCadena = cadena.length;
+    arreglo.forEach(element => {
+        if (element.length>tamañoCadena) {
+            respuesta.push(element);
+        }
+    });
+    return respuesta;
+}
+console.log(Ej1(cadena,arreglo));
+
+
+//let myArray=['insecto', 'bootcamp', 'escritorio'];
+let myArray=Ej1(cadena,arreglo);
+let lista=document.getElementById("lista");
+
+function arreglos(myArray){
+    myArray.forEach(element => {
+        lista.insertAdjacentHTML("beforeend",`
+        <li>${element}</li>
+        `)
+    });
+
+}
+arreglos(myArray);
